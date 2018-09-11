e16caef3e32fb82e899223ad37b8d39dee1632bf
RooperK <43035062+RooperK@users.noreply.github.com>
2018-09-11 13:06:58 +0300

    Add files via upload

---
+++ src/main/lab2/RooperK.java
@@ -0,0 +1,14 @@
+
+public class RooperK
+{
+    public static void main(String[]arg)
+        {
+            int a = 1;
+            short b = 2;
+            byte c = 127;
+            double d = 1000000000;
+            float f = 3.14f;
+            String str = "Попов_ИВТ/б-11о";
+            System.out.printf("Привет,%s:a=%7d,b=%o,c=%x,d=%+8.2f,f=%06.3f!",str,a,b,c,d,f);
+        }
+}
